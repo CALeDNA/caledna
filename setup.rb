38094a5a651f68a473f9cf1c2f450a2654b1e8dc
wykhuh <wykhuh@users.noreply.github.com>
2017-12-27 15:45:41 -0600

    add setup.rb

---
+++ setup.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+setup :secret_key_base
+env 'HOST', provide('Host', default: 'localhost:3000')
+env 'ASSET_HOST', provide('Asset host', default: '//localhost:3000')
+
+rake 'db:setup'
