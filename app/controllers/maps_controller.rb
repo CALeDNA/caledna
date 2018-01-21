7e41729a58df69fd5ec83b1f96605115ff779375
wykhuh <wykhuh@users.noreply.github.com>
2018-01-21 13:18:57 -0800

    add map link to samples status on Project#show

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -5,7 +5,15 @@ class MapsController < ApplicationController
     @markers =
       Sample.joins(:project)
             .select(:latitude, :longitude, :bar_code, :id, :submission_date,
-                    'projects.name', :project_id)
+                    'projects.name', :project_id, :status_cd)
+            .where(query_string)
             .all
+  end
+
+  def query_string
+    query = {}
+    query[:status_cd] = params[:status] if params[:status]
+    query[:project_id] = params[:project_id] if params[:project_id]
+    query
   end
 end
