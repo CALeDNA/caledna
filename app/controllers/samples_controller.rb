55d85ccd44c93f15465051a70abbd95065ad3307
wykhuh <wykhuh@users.noreply.github.com>
2018-01-22 04:41:05 -0800

    display project or sample name on Samples#index

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -3,6 +3,7 @@
 class SamplesController < ApplicationController
   def index
     @samples = Sample.order(:bar_code).where(query_string).page params[:page]
+    @display_name = display_name
   end
 
   def show
@@ -10,6 +11,15 @@ class SamplesController < ApplicationController
   end
 
   private
+
+  # TODO: add test
+  def display_name
+    if params[:project_id]
+      Project.select(:name).find(params[:project_id]).name
+    elsif params[:sample_id]
+      Sample.select(:bar_code).find(params[:sample_id]).bar_code
+    end
+  end
 
   def query_string
     query = {}
