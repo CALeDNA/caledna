020d60a36d95c0f803ce15a04aea7809897dcfe5
wykhuh <wykhuh@users.noreply.github.com>
2018-02-04 12:12:15 -0800

    display found specimens on Sample#show

--- app/models/taxonomic_unit.rb
+++ app/models/taxonomic_unit.rb
@@ -8,4 +8,8 @@ class TaxonomicUnit < ApplicationRecord
   has_many :specimens, foreign_key: 'tsn'
 
   scope :valid, -> { where(n_usage: 'valid').or(where(n_usage: 'accepted')) }
+
+  def common_name
+    vernaculars.where(language: 'English').pluck(:vernacular_name).join(', ')
+  end
 end
