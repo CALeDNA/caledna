19be7c41f4220266e720443b98cb8407d39df083
wykhuh <wykhuh@users.noreply.github.com>
2018-02-21 06:38:45 -0800

    convert language codes to name

--- app/models/vernacular.rb
+++ app/models/vernacular.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+# require_relative './languages'
+
 class Vernacular < ApplicationRecord
+  require 'languages'
+
   belongs_to :taxon, foreign_key: 'taxonID'
+
+  def language_name
+    return language if language.blank?
+    ::Languages::CODES[language.to_sym]
+  end
 end
