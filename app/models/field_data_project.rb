40a86ad905b1219ce651fe21b765a0ce9dffb328
wykhuh <wykhuh@users.noreply.github.com>
2018-05-03 18:53:53 -0700

    add Fieldwork Details tab to Sample#show

--- app/models/field_data_project.rb
+++ app/models/field_data_project.rb
@@ -2,6 +2,8 @@
 
 class FieldDataProject < ApplicationRecord
   DEFAULT_PROJECT = FieldDataProject.find_by(name: 'unknown')
+  MULTI_SAMPLE_FORM_IDS = [95_481, 87_534, 95_664, 83_937].freeze
+  SINGLE_SAMPLE_FORM_V1_IDS = [136_577, 130_560, 138_676, 170_620].freeze
 
   validates :kobo_id, uniqueness: true
 
@@ -19,5 +21,17 @@ class FieldDataProject < ApplicationRecord
 
   def results_completed_samples_count
     samples.select { |s| s.status_cd == 'results_completed' }.count
+  end
+
+  def multi_sample_form?
+    MULTI_SAMPLE_FORM_IDS.include?(kobo_id)
+  end
+
+  def single_sample_v1_form?
+    SINGLE_SAMPLE_FORM_V1_IDS.include?(kobo_id)
+  end
+
+  def single_sample_v2_form?
+    !(MULTI_SAMPLE_FORM_IDS + SINGLE_SAMPLE_FORM_V1_IDS).include?(kobo_id)
   end
 end
