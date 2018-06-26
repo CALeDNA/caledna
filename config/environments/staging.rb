27cba81014cd13229b78c97a5c858f533aee051d
wykhuh <wykhuh@users.noreply.github.com>
2018-06-26 08:23:25 -0700

    fix - change uglifier config for Heroku

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,4 +4,6 @@ require_relative 'production'
 
 Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
+  # https://github.com/lautis/uglifier/issues/127
+  config.assets.js_compressor = Uglifier.new(harmony: true)
 end
