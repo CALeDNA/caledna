9353552213459e04abc3521529f76272b50e4fc3
wykhuh <wykhuh@users.noreply.github.com>
2018-06-26 20:25:24 -0700

    add lograge to condense log messages to one line

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,15 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # use lograge to condense log messages to one line
+  config.lograge.enabled = true
+  config.lograge.custom_options = lambda do |event|
+    exceptions = %w(controller action format id)
+    {
+      params: event.payload[:params].except(*exceptions)
+    }
+  end
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
