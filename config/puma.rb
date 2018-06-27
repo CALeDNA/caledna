b8ab149f3424e50e3c03a7edf8555525465c25f2
wykhuh <wykhuh@users.noreply.github.com>
2018-06-27 06:07:22 -0700

    update puma config and Procfile

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+workers ENV.fetch('WEB_CONCURRENCY') { 2 }.to_i
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
@@ -8,6 +10,10 @@
 #
 threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }.to_i
 threads threads_count, threads_count
+
+preload_app!
+
+rackup DefaultRackup
 
 # Specifies the `port` that Puma will listen on to receive requests, default is
 # 3000.
@@ -46,5 +52,11 @@ environment ENV.fetch('RAILS_ENV') { 'development' }
 #   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
 # end
 
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
+
 # Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
+# plugin :tmp_restart
