e71a7520305ccf6f58cbd6e607a2de4afcb092c7
wykhuh <wykhuh@users.noreply.github.com>
2018-06-30 20:01:54 -0700

    clean up seed file
    - remove importing  taxon
    - remove fake sample, extractions, asvs

--- db/seed_data.rb
+++ db/seed_data.rb
@@ -4,12 +4,8 @@ module SeedData
   def delete_records
     puts 'deleting some records...'
 
-    Highlight.destroy_all
-    Asv.destroy_all
-    Photo.destroy_all
-    Extraction.destroy_all
+    Researcher.destroy_all
     ExtractionType.destroy_all
-    Sample.destroy_all
     FieldDataProject.destroy_all
   end
 
@@ -30,40 +26,7 @@ module SeedData
     )
   end
 
-  # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
-  def seed_samples(project)
-    puts 'seeding samples...'
-
-    FactoryBot.create_list(
-      :sample, 15,
-      field_data_project: project,
-      status: :submitted,
-      submission_date: Time.zone.now - 2.months
-    )
-
-    FactoryBot.create_list(
-      :sample, 5,
-      field_data_project: project,
-      status: :processing_sample,
-      submission_date: Time.zone.now - 2.months
-    )
-
-    FactoryBot.create_list(
-      :sample, 50,
-      field_data_project: project,
-      status: :results_completed,
-      submission_date: Time.zone.now - 2.months
-    )
-
-    Sample.all.each_with_index do |sample, i|
-      sample.update(
-        barcode: "K055#{i}-LC-S2",
-        latitude: "37.#{i * i}6783",
-        longitude: "-120.#{i * 2}23574"
-      )
-    end
-  end
-
+  # rubocop:disable Metrics/MethodLength
   def seed_people
     puts 'seeding people...'
     if Researcher.count.zero?
@@ -107,6 +70,7 @@ module SeedData
       processor2: processor2
     }
   end
+  # rubocop:enable Metrics/MethodLength
 
   def seed_extraction_types
     puts 'seeding extraction types...'
@@ -115,83 +79,5 @@ module SeedData
       :extraction_type,
       name: 'default'
     )
-  end
-
-  def seed_extractions(processor1, processor2, director)
-    puts 'seeding extractions...'
-
-    type_a = FactoryBot.create(:extraction_type, name: 'extraction A')
-    type_b = FactoryBot.create(:extraction_type, name: 'extraction B')
-
-    Sample.processing_sample.each do |sample|
-      processor = [processor1, processor2].sample
-      FactoryBot.create(
-        :extraction,
-        :processing_sample,
-        sample: sample,
-        processor_id: processor.id,
-        extraction_type: type_a,
-        sra_adder_id: director.id,
-        local_fastq_storage_adder_id: director.id,
-        status_cd: :processing_sample
-      )
-    end
-
-    Sample.results_completed.each do |sample|
-      processor = [processor1, processor2].sample
-      FactoryBot.create(
-        :extraction,
-        :results_completed,
-        sample: sample,
-        processor_id: processor.id,
-        extraction_type: type_a,
-        sra_adder_id: director.id,
-        local_fastq_storage_adder_id: director.id,
-        status_cd: :results_completed
-      )
-    end
-
-    Sample.results_completed.take(5).each do |sample|
-      processor = [processor1, processor2].sample
-      FactoryBot.create(
-        :extraction,
-        :results_completed,
-        sample: sample,
-        processor_id: processor.id,
-        extraction_type: type_b,
-        sra_adder_id: director.id,
-        local_fastq_storage_adder_id: director.id,
-        status_cd: :results_completed
-      )
-    end
-  end
-
-  def seed_asvs
-    puts 'seeding asv...'
-
-    vernacular_count = 10_000
-    taxon_count = 60_000
-    ids = []
-
-    rand(1..3).times do
-      taxon = Vernacular.offset(rand(vernacular_count)).take.taxon
-      ids.push(taxon.taxonID)
-    end
-
-    Extraction.all.each do |extraction|
-      rand(2..5).times do
-        taxon = Taxon.valid.offset(rand(taxon_count)).take
-        Asv.create(extraction: extraction, taxonID: taxon.taxonID)
-      end
-      Asv.create(extraction: extraction, taxonID: ids.sample)
-    end
-  end
-  # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
-
-  def seed_highlights
-    puts 'seeding highlights...'
-    Asv.limit(4).each do |asv|
-      Highlight.create(highlightable: asv, notes: Faker::Lorem.sentence)
-    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,45 +2,16 @@
 
 class DBSeeds
   require_relative('./seed_data')
-  require_relative('./seed_import')
   include SeedData
-  include SeedImport
 
   def seed_basic
     delete_records
-    reset_search
     seed_people
     seed_projects
     seed_extraction_types
     puts 'done seeding'
   end
-
-  # rubocop:disable Metrics/MethodLength
-  def seed_fake_data
-    delete_records
-    reset_search
-    people = seed_people
-    project = seed_projects
-    seed_extraction_types
-    seed_samples(project)
-    seed_extractions(people[:processor1], people[:processor2],
-                     people[:director])
-    seed_asvs
-    seed_highlights
-
-    puts 'done seeding'
-  end
-  # rubocop:enable Metrics/MethodLength
 end
-
-import_main = ENV.fetch('IMPORT_MAIN', 'true') == 'true'
-import_taxa = ENV.fetch('IMPORT_TAXA', 'false') == 'true'
-import_taxa_datasets = ENV.fetch('IMPORT_TAXA_DATASETS', 'false') == 'true'
 
 db_seeds = DBSeeds.new
 db_seeds.seed_basic
-
-has_taxa = Vernacular.count.positive?
-db_seeds.import_taxonomy_data if import_taxa && !has_taxa
-db_seeds.import_taxa_datasets if import_taxa_datasets
-db_seeds.seed_fake_data if !Rails.env.production? && import_main && has_taxa
