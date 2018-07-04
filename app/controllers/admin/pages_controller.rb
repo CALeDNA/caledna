3ce5115732abf134ddb1630e8b70458445df32cd
wykhuh <wykhuh@users.noreply.github.com>
2018-07-04 11:39:30 -0700

    fix: update router after creating or updating pages

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -5,6 +5,21 @@ module Admin
     require_relative './services/admin_text_editor'
     include AdminTextEditor
 
+    def create
+      super
+      Rails.application.reload_routes!
+    end
+
+    def update
+      super
+      Rails.application.reload_routes!
+    end
+
+    def destroy
+      super
+      Rails.application.reload_routes!
+    end
+
     layout :resolve_layout
   end
 end
