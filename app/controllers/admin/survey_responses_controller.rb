4f827c4d312135dcdca2459413d3cfc41b5e8565
wykhuh <wykhuh@users.noreply.github.com>
2018-07-22 15:52:00 -0700

    display user answer on SurveyResponses dashboard

--- app/controllers/admin/survey_responses_controller.rb
+++ app/controllers/admin/survey_responses_controller.rb
@@ -2,5 +2,14 @@
 
 module Admin
   class SurveyResponsesController < Admin::ApplicationController
+    include SurveysCalculateScore
+
+    def show
+      response = requested_resource
+      @user_answers = user_answers(response)
+      @correct_answers = correct_answers(response)
+      @survey = response.survey
+      super
+    end
   end
 end
