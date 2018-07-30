04ce4c559dbb299ed5fbcd73768c3673f5eeb83c
wykhuh <wykhuh@users.noreply.github.com>
2018-07-30 06:41:36 -0700

    fix primary key migration

--- db/migrate/20180729212954_change_external_resources_primary_key.rb
+++ db/migrate/20180729212954_change_external_resources_primary_key.rb
@@ -1,11 +1,11 @@
 class ChangeExternalResourcesPrimaryKey < ActiveRecord::Migration[5.2]
   def up
-    execute "ALTER TABLE external_resources DROP CONSTRAINT external_resources_pkey;"
+    # execute "ALTER TABLE external_resources DROP CONSTRAINT external_links_pkey;"
     add_column :external_resources, :id, :primary_key
   end
 
   def down
-    execute 'ALTER TABLE external_resources ADD PRIMARY KEY ("taxon_id");'
+    # execute 'ALTER TABLE external_resources ADD PRIMARY KEY ("taxon_id");'
     remove_column :external_resources, :id
   end
 end
