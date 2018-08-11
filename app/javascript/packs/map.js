0794a5d03db98eca8226a4723eb69b72206093be
wykhuh <wykhuh@users.noreply.github.com>
2018-08-11 09:28:09 -0700

    map v2: switch from circles to icons on zoom

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -3,28 +3,38 @@
   var initialLng = -118.3;
   var initialZoom = 6;
   var maxZoom = 18;
+  window.markerType = 'circle';
 
   var tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: maxZoom,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Points &copy 2012 LINZ'
   });
   var latlng = L.latLng(initialLat, initialLng);
-  var map = L.map('mapid', {center: latlng, zoom: initialZoom, layers: [tiles]});
+  var map = L.map('mapid-advance', {center: latlng, zoom: initialZoom, layers: [tiles]});
 
-  map.on('zoomend', function() {
+  map.on('zoomend', zoomHandler);
+
+  function zoomHandler () {
     var currentZoom = map.getZoom();
-    if (currentZoom > 15) {
-        map.removeLayer(icons);
-        map.addLayer(icons2);
+    var currentMarkerType = getMarkerType();
+    console.log('boo', currentZoom, currentMarkerType)
+
+    if(currentZoom > 9 && currentMarkerType == 'circle') {
+      addMarkers(samplesData, addIconMarker, 'icon')
+
+    } else if(currentZoom <= 9 && currentMarkerType == 'icon') {
+      addMarkers(samplesData, addCircleMarker, 'circle')
     }
-  });
+  }
 
-  $.get("/data/uc_reserves.geojson", function(data) {
 
-    L.geoJSON(JSON.parse(data)).addTo(map);
-  })
 
-  function addMarker(rawSample) {
+  function getMarkerType() {
+    console.log('getMarkerType', markerType)
+    return markerType
+  }
+
+  function formatSamplesData(rawSample) {
     var sample = rawSample.attributes;
     var lat = sample.latitude;
     var lng = sample.longitude;
@@ -42,10 +52,47 @@
       '<b>Status</b>: ' + status + '<br>' +
       '<b>Organism count</b>: ' + asvsCount + '<br>';
 
-    var marker = L.marker(L.latLng(lat, lng), { title: 'Sample ' + barcode });
-    marker.bindPopup(body);
-    map.addLayer(marker);
+    return {
+      lat: lat,
+      lng: lng,
+      barcode: barcode,
+      body: body
+
+    }
   }
+
+  var samplesData = []
+  var markers = []
+
+  function addIconMarker (sample) {
+    var marker = L.marker(L.latLng(sample.lat, sample.lng), { title: 'Sample ' + sample.barcode });
+    marker.bindPopup(sample.body);
+    map.addLayer(marker);
+    return marker;
+  }
+
+  function addCircleMarker (sample) {
+    var options = {fillColor: '#333', radius: 7, fillOpacity: .3, color: '#ccc', weight: 1};
+    var circleMarker = L.circleMarker(L.latLng(sample.lat, sample.lng), options)
+      .addTo(map);
+    circleMarker.bindPopup(sample.body);
+    return circleMarker;
+  }
+
+  function addMarkers(samplesData, markerFn, markerType) {
+    resetMarkers()
+    window.markerType = markerType;
+    console.log('addMarkers', window.markerType)
+    markers = samplesData.map(markerFn);
+  }
+
+  function resetMarkers() {
+    markers.forEach(function(marker){
+      map.removeLayer(marker);
+    });
+    markers = [];
+  }
+
 
   function onEachFeatureHandler(feature, layer) {
     if (feature.properties && feature.properties.AgencyName) {
@@ -64,10 +111,12 @@
   $.get( "/api/v1/samples", function(data) {
     var samples = data.samples.data;
 
-    samples.filter(function(rawSample) {
+    samplesData = samples.filter(function(rawSample) {
       var sample = rawSample.attributes;
       return sample.latitude && sample.longitude;
-    }).forEach(addMarker);
+    }).map(formatSamplesData)
+
+    addMarkers(samplesData, addCircleMarker, 'circle')
   });
 
 })()
