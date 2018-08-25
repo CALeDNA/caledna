d80ab48417f6f3a1e7065dcb91738cc7352f13e7
wykhuh <wykhuh@users.noreply.github.com>
2018-08-24 21:55:10 -0700

    set a limit to the number  matching taxa  displayed on Taxa#show

--- app/helpers/taxa_helper.rb
+++ app/helpers/taxa_helper.rb
@@ -6,12 +6,18 @@ module TaxaHelper
   end
 
   def self.format_matching_taxa(taxa)
+    max_limit = 25
     taxa_array = taxa.gsub(/[{}"]/, '').split(',')
 
-    taxa_array.map do |taxon|
+    results = taxa_array.take(max_limit).map do |taxon|
       name, id = taxon.split(' | ')
       path = Rails.application.routes.url_helpers.taxon_path(id: id)
       ActionController::Base.helpers.link_to(name, path)
     end.join(', ')
+
+    if taxa_array.length > max_limit
+      results += " ...#{taxa_array.length} matches"
+    end
+    results
   end
 end
