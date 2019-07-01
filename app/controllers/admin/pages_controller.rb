057008c2e891603748786e771a9727009fbcd8f8
wykhuh <wykhuh@users.noreply.github.com>
2019-07-01 06:04:34 -0700

    fix - make pagination work on admin Page model

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -5,6 +5,26 @@ module Admin
     require_relative './services/admin_text_editor'
     include AdminTextEditor
 
+    # NOTE: include index so I can use custom pagination params 'page_num'
+    # Adminstrate uses 'page' for pagination, which breaks when using Page model
+    def index
+      search_term = params[:search].to_s.strip
+      resources = Administrate::Search.new(scoped_resource,
+                                           dashboard_class,
+                                           search_term).run
+      resources = apply_collection_includes(resources)
+      resources = order.apply(resources)
+      resources = resources.page(params[:page_num]).per(records_per_page)
+      page = Administrate::Page::Collection.new(dashboard, order: order)
+
+      render locals: {
+        resources: resources,
+        search_term: search_term,
+        page: page,
+        show_search_bar: show_search_bar?,
+      }
+    end
+
     def create
       super
       Rails.application.reload_routes!
@@ -21,5 +41,13 @@ module Admin
     end
 
     layout :resolve_layout
+
+    private
+
+    def apply_collection_includes(relation)
+      resource_includes = []
+      return relation if resource_includes.empty?
+      relation.includes(*resource_includes)
+    end
   end
 end
