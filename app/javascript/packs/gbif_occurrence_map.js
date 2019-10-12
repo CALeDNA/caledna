060426ac5e038fc70faaaa26728537e5f80a5840
wykhuh <wykhuh@users.noreply.github.com>
2019-10-12 14:13:42 -0700

    refactor GBIF occurrence map on Taxa#show
    - move map from right column to underneath the eDNA map
    - make map work with IUCN status

--- app/javascript/packs/gbif_occurrence_map.js
+++ app/javascript/packs/gbif_occurrence_map.js
@@ -1,20 +1,21 @@
-var taxonId = window.caledna && window.caledna.gbif_id;
+const taxonId = window.caledna && window.caledna.gbif_id;
 
 if (taxonId) {
-  var map = L.map("gbif-occurrence-map", {
-    center: L.latLng(30, 0),
+  const map = L.map("gbif-occurrence-map", {
+    center: L.latLng(0, 0),
     zoom: 1,
+    minZoom: 1,
     maxZoom: 18
   });
 
-  // map tiles
+  // gbif map tiles
   L.tileLayer(
     "https://tile.gbif.org/3857/omt/{z}/{x}/{y}@2x.png?style=gbif-geyser"
   ).addTo(map);
 
   // gbif occurrences
-  var hexPerTile = 50;
-  var apiEndpoint =
+  const hexPerTile = 50;
+  const apiEndpoint =
     "https://api.gbif.org/v2/map/occurrence/density/{z}/{x}/{y}@2x.png?srs=EPSG:3857" +
     `&style=purpleYellow.poly&bin=hex&hexPerTile=${hexPerTile}&taxonKey=${taxonId}`;
   L.tileLayer(apiEndpoint).addTo(map);
