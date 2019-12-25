3023245d7cd160fdc3c5839bfa0b02215a7d8416
wykhuh <wykhuh@users.noreply.github.com>
2019-12-25 08:23:28 -0600

    remove Sample#location_display

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -5,7 +5,7 @@ class Sample < ApplicationRecord
   include InsidePolygon
 
   multisearchable against: %i[
-    barcode status_cd location_display field_project_name
+    barcode status_cd location field_project_name
     research_projects_names
   ]
 
@@ -64,6 +64,7 @@ class Sample < ApplicationRecord
     when 'nor' then 'Nor'
     when 'wet' then 'Wet'
     when 'wet_1' then 'Wet+'
+    else kobo_data['Moisture']
     end
   end
 
@@ -79,105 +80,7 @@ class Sample < ApplicationRecord
     when 'hgh' then 'Hgh-'
     when 'hgh_1' then 'Hgh'
     when 'hgh_2' then 'Hgh+'
-    end
-  end
-
-  def habitat_display
-    case kobo_data['habitat'] ||
-      kobo_data['_optional_Describe_ou_are_sampling_from']
-    when 'terrestrial' then 'Terrestrial habitat, not submerged'
-    when 'wetland' then 'Rarely submerged or wetland or arroyo'
-    when 'freq_submerged' then 'Frequently submerged or intertidal or marsh'
-    when 'full_submerged' then 'Fully submerged'
-    end
-  end
-
-  def depth_display
-    case kobo_data['depth_your_samples'] ||
-      kobo_data['_optional_What_dept_re_your_samples_from']
-    when 'Top' then 'Top layer (top 3cm) soil or sediment'
-    when 'Below' then 'Below top 3cm soil or sediment'
-    when 'Sub_3_to_30_cm' then 'Submerged 3-30cm '
-    when 'Sub_30_to_60_c_1' then 'Submerged 30-60cm'
-    when 'Sub_60_to_2m' then 'Submerged 60cm-2m'
-    when 'Sub_2_to_10m' then 'Submerged 2m-10m'
-    when 'Sub_10_to_50m' then 'Submerged 10m-50m'
-    when 'Sub_over_50m' then 'Submerged >50m'
-    end
-  end
-
-  def location_display
-    return if location.blank?
-    location.split(' ').map do |part|
-      case part
-      when 'UCNRS' then 'UC Natural Reserve'
-      when 'CVMSHCP' then 'Coachella Valley MSHCP site'
-      when 'AUTOMATIC_1' then nil
-      when 'AUTOMATIC_2' then nil
-      else part
-      end
-    end.compact.join(' ').tr('_', ' ')
-  end
-
-  # rubocop:disable Metrics/AbcSize
-  def cvmshcp_display
-    case kobo_data['Location']
-    when 'Cabazon' then 'Cabazon'
-    when 'CV_Stormwater' then 'CV Stormwater Channel'
-    when 'Desert_tortois' then 'Desert Tortoise'
-    when 'Dos_Palmas' then 'Dos Palmas'
-    when 'East_Indio' then 'East Indio Hills'
-    when 'Edom_Hill' then 'Edom Hill'
-    when '111_10' then 'Highway 111/I-10'
-    when 'Indio_Hills_Pa' then 'Indio Hills Palms'
-    when 'Indio_Joshua_L' then 'Indio/Joshua Tree Linkage'
-    when 'JoshTreeNatlPa' then 'Joshua Tree National Park'
-    when 'Long_Canyon' then 'Long Canyon'
-    when 'Mecca_Orocopia' then 'Mecca Hills / Orocopia'
-    when 'Santa_Rosa_San' then 'Santa Rosa and San Jacinto Mtns'
-    when 'Snowy_Creek_Wi' then 'Snow Creek Windy Point'
-    when 'Stubbe_Cottonw' then 'Stubbe and Cottonwood Canyons'
-    when 'Thousand_Palms' then 'Thousand Palms'
-    when 'Mission_Morong' then 'Upper Mission Creek / Big Morongo Canyon'
-    when 'West_Deception' then 'West Deception Canyon'
-    when 'Whitewater_Can' then 'Whitewater Canyon'
-    when 'Whitewater_Flo' then 'Whitewater Floodplain'
-    when 'Willow_Hole' then 'Willow Hole'
-    end
-  end
-  # rubocop:enable Metrics/AbcSize
-
-  def environmental_features_display
-    case kobo_data['environment_feature'] ||
-      kobo_data['Choose_from_common_environment']
-    when 'closed_water' then 'Enclosed water'
-    when 'estuary' then 'Estuary (partially enclosed)'
-    when 'open_water' then 'Open water'
-    when 'reef' then 'Reef'
-    when 'ridge' then 'Elevated ridge'
-    when 'slope' then 'Slope/slant'
-    when 'wash' then 'Basin/wash'
-    when 'mound' then 'Rock mound'
-    when 'pit' then 'Pit or ravine'
-    when 'terrace' then 'Flat land/terrace'
-    when 'shore' then 'Rocky shore'
-    when 'beach' then 'Beach'
-    when 'kelp_forest' then 'Kelp forest'
-    else 'Other'
-    end
-  end
-
-  def environmental_settings_display
-    case kobo_data['Describe_the_environ_tions_from_this_list']
-    when 'road' then 'On roadside'
-    when 'trail' then 'On trail'
-    when 'near_road' then 'Near (<5m) road or trail'
-    when 'near_trail' then 'Near (<5m) buildings'
-    when 'farm' then 'On farm'
-    when 'garden' then 'On garden'
-    when 'manmade' then 'On manmade landscape'
-    when 'near_stagnant_' then 'Near (<2m) stagnant water'
-    when 'near_moving_wa' then 'Near (<2m) moving water'
+    else kobo_data['light'] || kobo_data['Light']
     end
   end
   # rubocop:enable Metrics/CyclomaticComplexity, Metrics/MethodLength
--- app/serializers/sample_serializer.rb
+++ app/serializers/sample_serializer.rb
@@ -4,8 +4,4 @@ class SampleSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :latitude, :longitude, :barcode, :status, :substrate,
              :primers, :gps_precision, :location
-
-  attribute :location do |object|
-    object.location_display
-  end
 end
