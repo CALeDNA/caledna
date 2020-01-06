1a164cee150227a6fb7581cdca556806f694ff10
wykhuh <wykhuh@users.noreply.github.com>
2020-01-05 18:02:21 -0600

    edit previous create primers migration; don't create primers records
    - don’t create primers records because they mess up running tests

--- db/migrate/20191014141941_create_primers.rb
+++ db/migrate/20191014141941_create_primers.rb
@@ -1,5 +1,5 @@
 class CreatePrimers < ActiveRecord::Migration[5.2]
-  def up
+  def change
     create_table :primers do |t|
       t.string :name, null: false
       t.text :sequence_1
@@ -8,13 +8,5 @@ class CreatePrimers < ActiveRecord::Migration[5.2]
 
       t.timestamps
     end
-
-    %w[12S 16S 18S PITS FITS CO1 trnL].each do |name|
-      Primer.create(name: name)
-    end
-  end
-
-  def down
-    drop_table :primers
   end
 end
