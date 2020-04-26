4cbecc7f2e00a1833597f9e94b4e91cea5902559
wykhuh <wykhuh@users.noreply.github.com>
2020-04-25 22:14:59 -0700

    show "other" NcbiNames on TaxaShow names

--- app/models/ncbi_name.rb
+++ app/models/ncbi_name.rb
@@ -5,6 +5,10 @@ class NcbiName < ApplicationRecord
     where("name_class != 'common name' AND name_class != 'genbank common name'")
   end)
 
+  scope :other, (lambda do
+    where("name_class != 'scientific name'")
+  end)
+
   def ncbi_node
     NcbiNode.find_by(ncbi_id: taxon_id)
   end
