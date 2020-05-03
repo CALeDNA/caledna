e523a4d645d5669e756105e63d8d64217e5ebb1f
wykhuh <wykhuh@users.noreply.github.com>
2020-05-03 02:15:03 -0700

    add <hr> button to summernote

--- app/assets/javascripts/text_editor.js
+++ app/assets/javascripts/text_editor.js
@@ -1,87 +1,106 @@
 //= require bootstrap-sprockets
 //= require summernote/summernote-bs4.min
 
-(function() {
-  const sendFile = function(file, toSummernote) {
+(function () {
+  const sendFile = function (file, toSummernote) {
     var data;
-    data = new FormData;
-    data.append('upload[image]', file);
+    data = new FormData();
+    data.append("upload[image]", file);
 
     $.ajaxSetup({
-      headers:
-      { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') }
+      headers: { "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content") },
     });
 
     return $.ajax({
       data: data,
-      type: 'POST',
-      url: '/uploads',
+      type: "POST",
+      url: "/uploads",
       cache: false,
       contentType: false,
       processData: false,
-      success: function(data) {
-        console.log('file uploading...');
-        if (typeof data.errors !== 'undefined' && data.errors !== null) {
-          console.log('ops! errors...', data.errors);
+      success: function (data) {
+        console.log("file uploading...");
+        if (typeof data.errors !== "undefined" && data.errors !== null) {
+          console.log("ops! errors...", data.errors);
         } else {
-          console.log('inserting image in to editor...', data.url);
-          return toSummernote.summernote('insertImage', data.url, ($image) => {
-            $image.attr('data-id', data.id);
+          console.log("inserting image in to editor...", data.url);
+          return toSummernote.summernote("insertImage", data.url, ($image) => {
+            $image.attr("data-id", data.id);
           });
         }
-      }
+      },
     });
   };
 
-  const deleteFile = function(file_id) {
+  const deleteFile = function (file_id) {
     return $.ajax({
-      type: 'DELETE',
+      type: "DELETE",
       url: "/uploads/" + file_id,
       cache: false,
       contentType: false,
-      processData: false
+      processData: false,
     });
   };
 
-  Array.prototype.diff = function(a) {
-    return this.filter(function(i) {
+  Array.prototype.diff = function (a) {
+    return this.filter(function (i) {
       return a.indexOf(i) < 0;
     });
   };
 
-  $(function() {
-    return $('[data-provider="summernote"]').each(function() {
+  $(function () {
+    return $('[data-provider="summernote"]').each(function () {
       return $(this).summernote({
         toolbar: [
-          ['styles', ['style', 'bold', 'italic', 'underline', 'superscript', 'subscript', 'clear']],
-          ['blocks', ['ul', 'ol', 'paragraph']],
-          ['insert', ['link', 'picture', 'video']],
-          ['misc', ['fullscreen', 'codeview', 'help']],
+          [
+            "styles",
+            [
+              "style",
+              "bold",
+              "italic",
+              "underline",
+              "superscript",
+              "subscript",
+              "clear",
+            ],
+          ],
+          ["blocks", ["ul", "ol", "paragraph", "hr"]],
+          ["insert", ["link", "picture", "video"]],
+          ["misc", ["fullscreen", "codeview", "help"]],
         ],
-        lang: 'ko-KR',
+        lang: "ko-KR",
         height: 400,
         callbacks: {
-          onInit: function() {
-            console.log('Summernote is launched');
-            return this.oldValue = this.value;
+          onInit: function () {
+            console.log("Summernote is launched");
+            return (this.oldValue = this.value);
           },
-          onImageUpload: function(files, e) {
+          onImageUpload: function (files, e) {
             console.log("Files were uploaded: ");
             console.log(files);
             return sendFile(files[0], $(this));
           },
-          onMediaDelete: function(target, editor, editable) {
-            var upload_id = target.attr('data-id');
+          onMediaDelete: function (target, editor, editable) {
+            var upload_id = target.attr("data-id");
             console.log("File was deleted : " + upload_id);
 
             if (!!upload_id) {
               deleteFile(upload_id);
-              this.oldValue = $('.note-editable.card-block')[0].innerHTML;
+              this.oldValue = $(".note-editable.card-block")[0].innerHTML;
             }
             return target.remove();
           },
-          onKeyup: function(e) {
-            var deletedImage, deletedImages, matches, myRegexp, newImages, newValue, oldImages, _i, _len, _results;
+          onKeyup: function (e) {
+            var deletedImage,
+              deletedImages,
+              matches,
+              myRegexp,
+              newImages,
+              newValue,
+              oldImages,
+              _i,
+              _len,
+              _results;
             if (e.keyCode === 8 || e.keyCode === 46) {
               newValue = e.target.innerHTML;
               oldImages = this.oldValue.match(/<img\s(?:.+?)>/g);
@@ -94,11 +113,22 @@
                 _results = [];
                 for (_i = 0, _len = deletedImages.length; _i < _len; _i++) {
                   deletedImage = deletedImages[_i];
-                  myRegexp= /data-id="(\d+)"/g;
+                  myRegexp = /data-id="(\d+)"/g;
                   matches = myRegexp.exec(deletedImage);
-                  if (confirm("Are you sure?\nYou can't revert if images have been deleted.")) {
+                  if (
+                    confirm(
+                      "Are you sure?\nYou can't revert if images have been deleted."
+                    )
+                  ) {
                     deleteFile(matches[1]);
-                    _results.push(console.log("* Permanently removed : " + matches[1] + ": " + matches[2]));
+                    _results.push(
+                      console.log(
+                        "* Permanently removed : " +
+                          matches[1] +
+                          ": " +
+                          matches[2]
+                      )
+                    );
                   } else {
                     _results.push(void 0);
                   }
@@ -106,10 +136,9 @@
                 return _results;
               }
             }
-          }
-        }
+          },
+        },
       });
     });
   });
-
-}).call(this);
+}.call(this));
