810fd53ed2c9e525785d6b35028f5710b9c3e5c7
wykhuh <wykhuh@users.noreply.github.com>
2020-07-13 09:51:52 -0700

    update Sample#geom when batch changing longitude sign

--- spec/requests/admin/labwork/batch_actions_controller_spec.rb
+++ spec/requests/admin/labwork/batch_actions_controller_spec.rb
@@ -18,16 +18,27 @@ describe 'BatchActionController' do
     end
 
     describe '#POST change_longitude_sign' do
+      def point_factory(lon, lat)
+        RGeo::Cartesian.preferred_factory(srid: 3785).point(lon, lat)
+      end
+
       it 'changes multiple sample longitude' do
-        sample1 = create(:sample, barcode: 'KOOO1', id: 1, longitude: 1)
-        sample2 = create(:sample, barcode: 'KOOO2', id: 2, longitude: 1)
+        sample1 = create(:sample, barcode: 'KOOO1', id: 1,
+                                  longitude: 1, latitude: 2)
+        sample2 = create(:sample, barcode: 'KOOO2', id: 2,
+                                  longitude: 3, latitude: 4)
         params = { batch_action: { ids: [1, 2] } }
         path = admin_labwork_batch_change_longitude_sign_path
 
         expect { post path, params: params }
           .to change { sample1.reload.longitude }
           .to(-1)
-          .and change { sample2.reload.longitude }.to(-1)
+          .and change { sample1.reload.geom }
+          .to(point_factory(-1, 2))
+          .and change { sample2.reload.longitude }
+          .to(-3)
+          .and change { sample2.reload.geom }
+          .to(point_factory(-3, 4))
       end
     end
   end
