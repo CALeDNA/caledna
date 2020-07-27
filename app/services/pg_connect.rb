8f2c3aa8c91067e635355eaa57f979a5923208eb
wykhuh <wykhuh@users.noreply.github.com>
2020-07-27 11:09:25 -0700

    add PGConnect to handle raw sql queries

---
+++ app/services/pg_connect.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class PgConnect
+  def self.execute(sql)
+    db_string = if Rails.env.production?
+                  'DATABASE_URL'
+                elsif Rails.env.testing?
+                  'DATABASE_TEST'
+                else
+                  'DATABASE_DEV'
+                end
+    begin
+      con = PG.connect(ENV.fetch(db_string))
+      con.exec(sql)
+    rescue PG::Error => e
+      puts e.message
+    ensure
+      con&.close
+    end
+  end
+end
