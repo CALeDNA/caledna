5e6f0de59746858cb2d994274131df5b93732ce6
wykhuh <wykhuh@users.noreply.github.com>
2020-08-24 09:59:19 -0700

    fix - delete all related records when deleting research project eDNA results
    delete sample_primers and research_project_sources

--- app/controllers/admin/tasks/research_project_results_controller.rb
+++ app/controllers/admin/tasks/research_project_results_controller.rb
@@ -11,10 +11,13 @@ module Admin
                                    .collect { |p| [p.name, p.id] }
       end
 
+      # rubocop:disable Metrics/MethodLength
       def bulk_delete
         authorize 'AdminDashboard'.to_sym, :admin?
         if project_id.present?
-          delete_asvs
+          ActiveRecord::Base.transaction do
+            delete_records
+          end
           flash[:success] = 'Research project eDNA results were deleted.'
           redirect_to admin_research_project_edna_results_path(project_id)
         else
@@ -22,6 +25,7 @@ module Admin
           redirect_to admin_labwork_bulk_delete_research_project_results_path
         end
       end
+      # rubocop:enable Metrics/MethodLength
 
       private
 
@@ -29,10 +33,49 @@ module Admin
         bulk_delete_params['research_project_id']
       end
 
-      def delete_asvs
-        sql = 'DELETE FROM asvs WHERE research_project_id = $1'
+      def delete_records
+        delete_asv
+        delete_research_project_sources
+        delete_sample_primers
+        update_sample_status
+      end
+
+
+      def update_sample_status
+        sql = <<~SQL
+          UPDATE samples SET status_cd = 'approved' WHERE id IN (
+            SELECT id FROM samples WHERE status_cd = 'results_completed'
+            EXCEPT
+            SELECT  sample_id FROM sample_primers
+           );
+        SQL
+
         bindings = [[nil, project_id]]
-        ActiveRecord::Base.connection.exec_query(sql, 'q', bindings)
+        execute(sql, 'q', bindings)
+      end
+
+      def delete_asv
+        sql = 'DELETE FROM asvs WHERE research_project_id = $1;'
+
+        bindings = [[nil, project_id]]
+        execute(sql, 'q', bindings)
+      end
+
+      def delete_research_project_sources
+        sql = 'DELETE FROM research_project_sources WHERE ' \
+          'research_project_id = $1'
+        bindings = [[nil, project_id]]
+        execute(sql, 'q', bindings)
+      end
+
+      def delete_sample_primers
+        sql = 'DELETE FROM sample_primers WHERE research_project_id = $1;'
+        bindings = [[nil, project_id]]
+        execute(sql, 'q', bindings)
+      end
+
+      def execute(sql, bindings = [])
+        ActiveRecord::Base.connection.exec_query(sql, bindings)
       end
 
       def bulk_delete_params
