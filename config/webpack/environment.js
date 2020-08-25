2bea18f63b2ae77a62cff22bbb2a6bb3a543b95c
wykhuh <wykhuh@users.noreply.github.com>
2020-08-25 11:57:39 -0700

    update vue-loader

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,9 @@
 const { environment } = require('@rails/webpacker')
 const vue =  require('./loaders/vue')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 environment.loaders.append('vue', vue)
+environment.plugins.append('VueLoaderPlugin', new VueLoaderPlugin())
+
 module.exports = environment
+
--- config/webpack/loaders/vue.js
+++ config/webpack/loaders/vue.js
@@ -1,13 +1,17 @@
-const { dev_server: devServer } = require('@rails/webpacker').config
-
-const isProduction = process.env.NODE_ENV === 'production'
-const inDevServer = process.argv.find(v => v.includes('webpack-dev-server'))
-const extractCSS = !(inDevServer && (devServer && devServer.hmr)) || isProduction
+const { dev_server: devServer } = require("@rails/webpacker").config;
+const isProduction = process.env.NODE_ENV === "production";
+const inDevServer = process.argv.find((v) => v.includes("webpack-dev-server"));
+const extractCSS = !(inDevServer && devServer && devServer.hmr) || isProduction;
 
 module.exports = {
-  test: /\.vue(\.erb)?$/,
-  use: [{
-    loader: 'vue-loader',
-    options: { extractCSS }
-  }]
-}
+  test: /\.vue$/,
+  use: [
+    {
+      loader: "vue-loader",
+      options: {
+        extractCSS,
+      },
+    },
+  ],
+};
+
