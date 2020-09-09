027e8eea0116f6d2980284369b6fd82b8e7fccc1
wykhuh <wykhuh@users.noreply.github.com>
2020-09-08 17:24:46 -0700

    fix - make webfonts work Cloudfront with CORS

--- config/initializers/rack_cors.rb
+++ config/initializers/rack_cors.rb
@@ -2,8 +2,8 @@ if defined? Rack::Cors
   Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins %w[
-        https://data.ucedna.com
-         http://data.ucedna.com
+        https://www.protectingourriver.org
+        http://www.protectingourriver.org
       ]
 
       resource '/assets/*',
