b7be61fe36081d3b41f4ac577cbacb0dc532732f
wykhuh <wykhuh@users.noreply.github.com>
2020-09-09 15:27:19 -0700

    add UserSubmission to admin

---
+++ app/dashboards/user_submission_dashboard.rb
@@ -0,0 +1,70 @@
+require "administrate/base_dashboard"
+
+class UserSubmissionDashboard < Administrate::BaseDashboard
+  ATTRIBUTE_TYPES = {
+    id: Field::Number,
+    user: Field::BelongsTo,
+    user_display_name: Field::String,
+    title: Field::String,
+    user_bio: TextEditorField,
+    content: TextEditorField,
+    media_url: Field::String,
+    twitter: Field::String,
+    facebook: Field::String,
+    instagram: Field::String,
+    website: Field::String,
+    approved: Field::Boolean,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime,
+    image: ActiveStorageAttachmentField,
+  }.freeze
+
+  COLLECTION_ATTRIBUTES = %i[
+  id
+  user
+  title
+  approved
+  created_at
+  ].freeze
+
+  SHOW_PAGE_ATTRIBUTES = %i[
+  id
+  approved
+  user_display_name
+  title
+  user_bio
+  content
+  image
+  media_url
+  twitter
+  facebook
+  instagram
+  website
+  created_at
+  updated_at
+  ].freeze
+
+
+  FORM_ATTRIBUTES = %i[
+  approved
+  user
+  user_display_name
+  title
+  user_bio
+  content
+  image
+  media_url
+  twitter
+  facebook
+  instagram
+  website
+  ].freeze
+
+
+  COLLECTION_FILTERS = {}.freeze
+
+
+  def display_resource(user_submission)
+    user_submission.title
+  end
+end
---
+++ app/policies/user_submission_policy.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class UserSubmissionPolicy < ApplicationPolicy
+  def index?
+    admin_roles
+  end
+
+  def show?
+    admin_roles
+  end
+
+  def create?
+    admin_roles
+  end
+
+  def update?
+    admin_roles
+  end
+
+  def destroy?
+    admin_roles
+  end
+end
