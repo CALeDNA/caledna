49b923105702a954827655be386e72752244fd17
wykhuh <wykhuh@users.noreply.github.com>
2020-09-23 05:50:03 -0700

    RiverExplorer - display PouR locations

--- app/javascript/components/river-explorer.vue
+++ app/javascript/components/river-explorer.vue
@@ -22,6 +22,8 @@
 </template>
 
 <script>
+  import axios from "axios";
+
   import {
   import AnalyteList from "./shared/components/analyte-list";
   import { createLARWMP2018 } from "../packs/river_explorer_map";
@@ -30,6 +32,8 @@
     createRiverLayer,
     createWatershedLayer,
   } from "../packs/river_explorer_map";
+  import base_map from "../packs/base_map";
+
   export default {
     name: "RiverExplorer",
     components: {
@@ -38,8 +42,12 @@
     data: function() {
       return {
         activeTab: "mapTab",
+        taxaKeyword: null,
         map: null,
         selectedLayers: {},
+        pourLocationsLayer: null,
+      };
+    },
     methods: {
       toggleJsonLayer: function(layerName, json) {
         if (this.selectedLayers[layerName]) {
@@ -50,14 +58,42 @@
           this.map.addLayer(this.selectedLayers[layerName]);
         }
       },
+      toggleMapMarkerLayer(layerName, mapLayer) {
+        if (this.selectedLayers[layerName]) {
+          this.map.removeLayer(mapLayer);
+          this.selectedLayers[layerName] = null;
+        } else {
+          this.selectedLayers[layerName] = mapLayer;
+          this.map.addLayer(mapLayer);
+        }
+      },
       appendSelectedLayers: function(layer) {
-        if (layer !== "LARWMP (2018)") {
+        if (layer == "LARWMP (2018)" || layer == "PouR") {
+        } else {
           this.selectedLayers[layer] = true;
         }
 
         if (layer == "LARWMP (2018)") {
-          this.toggleJsonLayer("LARWMP2018Layer", createLARWMP2018());
+          this.toggleJsonLayer(layer, createLARWMP2018());
+        } else if (layer == "PouR") {
+          this.toggleMapMarkerLayer(layer, this.pourLocationsLayer);
         }
+      },
+      showInfo: function(layer) {
+        alert(`Info about ${layer}`);
+      },
+      fetchPourLocations: function() {
+        axios
+          .get("/api/v1/places_basic?place_type=pour_location")
+          .then((response) => {
+            this.pourLocationsLayer = base_map.createMarkerLayer(
+              response.data.places,
+              base_map.createCircleMarker
+            );
+          })
+          .catch((e) => {
+            console.error(e);
+          });
       },
     },
     mounted: function() {
@@ -65,6 +101,7 @@
         this.map = initMap();
         this.map.addLayer(createWatershedLayer());
         this.map.addLayer(createRiverLayer());
+        this.fetchPourLocations();
       });
     },
   };
