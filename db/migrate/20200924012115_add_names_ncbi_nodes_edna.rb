4c610d799fa32851d0233de3c92447a1bf396942
wykhuh <wykhuh@users.noreply.github.com>
2020-09-23 18:44:57 -0700

    add names to ncbi_nodes_edna

---
+++ db/migrate/20200924012115_add_names_ncbi_nodes_edna.rb
@@ -0,0 +1,30 @@
+class AddNamesNcbiNodesEdna < ActiveRecord::Migration[5.2]
+  def up
+    execute('DROP MATERIALIZED VIEW IF EXISTS ncbi_nodes_edna;')
+
+    sql =<<~SQL
+      CREATE MATERIALIZED VIEW ncbi_nodes_edna AS
+      SELECT taxon_id, rank, canonical_name,asvs_count, asvs_count_la_river,
+      iucn_status, ids, division_id, cal_division_id, names
+      FROM ncbi_nodes
+      WHERE  taxon_id IN (SELECT taxon_id FROM asvs)
+      AND (
+        ncbi_nodes.iucn_status IS NULL OR
+        ncbi_nodes.iucn_status NOT IN
+        ('#{IucnStatus::THREATENED.values.join("','")}')
+      )
+    SQL
+    execute(sql)
+
+    add_index :ncbi_nodes_edna, :taxon_id
+    add_index :ncbi_nodes_edna, :asvs_count
+    add_index :ncbi_nodes_edna, :asvs_count_la_river
+    add_index :ncbi_nodes_edna, :ids, using: :gin
+    add_index :ncbi_nodes_edna, :cal_division_id
+    add_index :ncbi_nodes_edna, :names, using: :gin
+  end
+
+  def down
+    execute('DROP MATERIALIZED VIEW IF EXISTS ncbi_nodes_edna;')
+  end
+end
