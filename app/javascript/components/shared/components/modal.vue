1e24062786848b30afa89bdf538888f52644e65c
wykhuh <wykhuh@users.noreply.github.com>
2020-09-25 06:38:37 -0700

    add modal using Vue

---
+++ app/javascript/components/shared/components/modal.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="my-modal-mask">
+    <div class="modal-wrapper">
+      <div class="modal-container">
+        <div class="modal-header">
+          <button
+            class="modal-default-button btn btn-default"
+            @click="$emit('close')"
+          >
+            ×
+          </button>
+          <slot name="header">
+            default header
+          </slot>
+        </div>
+
+        <div class="modal-body">
+          <slot name="body">
+            default body
+          </slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: "modal",
+  };
+</script>
