ae46485b31b799e7e85afe5c29d89367ec00806c
wykhuh <wykhuh@users.noreply.github.com>
2020-09-25 05:54:11 -0700

    RE - add modal

--- app/javascript/components/shared/components/analyte-list.vue
+++ app/javascript/components/shared/components/analyte-list.vue
@@ -1,28 +1,42 @@
 <template>
-  <ul>
-    <li v-for="(value, key) in list" v-bind:key="key">
-      <input
-        type="checkbox"
-        :id="key"
-        :name="key"
-        :value="key"
-        @click="addLayer(key, $event)"
-      />
-      <label :for="key">{{ key }} </label>
-      <span @click="showInfo(key)">
-        <i class="far fa-question-circle"></i>
-      </span>
-    </li>
-  </ul>
+  <div>
+    <ul class="analyte-list">
+      <li v-for="(value, key) in list" v-bind:key="key">
+        <input
+          type="checkbox"
+          :id="key"
+          :name="key"
+          :value="key"
+          @click="addLayer(key, $event)"
+        />
+        <label :for="key">{{ key }}</label>
+        <span @click="showModal(key)">
+          <i class="far fa-question-circle"></i>
+        </span>
+        <Modal v-if="key == currentModal" @close="currentModal = null">
+          <h3 slot="header">{{ key }}</h3>
+          <div slot="body">TODO: Add info about {{ key }}</div>
+        </Modal>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+  import Modal from "./modal";
+
   export default {
     name: "AnalyteList",
+    components: {
+      Modal,
+    },
     props: ["list"],
+    data: function() {
+      return { currentModal: null };
+    },
     methods: {
-      showInfo: function(layer) {
-        alert(`TODO: Add info about ${layer}`);
+      showModal: function(layer) {
+        this.currentModal = layer;
       },
       addLayer: function(layer, event) {
         this.$emit("addSelectedLayer", layer, event.target.checked);
