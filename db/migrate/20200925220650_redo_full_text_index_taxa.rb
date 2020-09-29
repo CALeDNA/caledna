2f03ce3a018acb31ce3be1eec4baf3386652fbee
wykhuh <wykhuh@users.noreply.github.com>
2020-09-28 17:11:58 -0700

    update migration

--- db/migrate/20200925220650_redo_full_text_index_taxa.rb
+++ db/migrate/20200925220650_redo_full_text_index_taxa.rb
@@ -1,6 +1,6 @@
 class RedoFullTextIndexTaxa < ActiveRecord::Migration[5.2]
   def change
-    remove_index :ncbi_nodes, :full_text_search_idx
+    execute 'DROP INDEX IF EXISTS full_text_search_idx '
     sql = <<~SQL
     CREATE INDEX full_text_search_idx ON ncbi_nodes USING gin(
       (to_tsvector('simple', canonical_name) || to_tsvector('english', common_names))
