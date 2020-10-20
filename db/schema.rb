4d840fb046cec938277cbbb8e603eb3af1b9fc07
wykhuh <wykhuh@users.noreply.github.com>
2020-10-19 23:54:18 -0700

    rake: normalize wikidata images for external resources

--- db/schema.rb
+++ db/schema.rb
@@ -114,6 +114,7 @@ ActiveRecord::Schema.define(version: 2020_10_20_002550) do
     t.text "wiki_excerpt"
     t.string "wiki_title"
     t.boolean "active", default: true
+    t.jsonb "dup_data", default: {}
     t.index ["gbif_id"], name: "index_external_resources_on_gbif_id"
     t.index ["ncbi_id"], name: "index_external_resources_on_ncbi_id"
     t.index ["search_term"], name: "index_external_resources_on_search_term"
