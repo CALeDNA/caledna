d849eea338561bd3d2dee59eb7ebcb6dd6e08564
wykhuh <wykhuh@users.noreply.github.com>
2020-10-29 14:46:10 -0700

    update docs

--- docs/queries/misc.sql
+++
@@ -1,4 +0,0 @@
--- create clean_taxonomy_string_phylum by removing
--- superkingdom from clean_taxonomy_string
-
-UPDATE result_taxa set clean_taxonomy_string_phylum = regexp_replace(clean_taxonomy_string, '^.*?;' , '');
