bd674595ced34f473925042389387f506e71ea4f
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-05-02 23:29:49 -0700

    toggle location and samples markers on Places#index

--- app/javascript/components/places-index-map.vue
+++ app/javascript/components/places-index-map.vue
@@ -1,6 +1,45 @@
 <template>
   <div class="row">
     <div class="col col-md-4">
+      <div class="taxa-markers">
+        <div>
+          <input
+            type="checkbox"
+            value="taxa-sites"
+            v-model="showPlacesLayer"
+            @click="togglePlacesLayer"
+          />
+          <svg height="30" width="30" @click="togglePlacesLayer">
+            <polygon
+              points="15 9, 22 22, 8 22"
+              stroke="#222"
+              stroke-width="2"
+              fill="#5aa172"
+            />
+          </svg>
+          {{ placesCount }} {{ "location" | pluralize(placesCount) }}
+        </div>
+        <div>
+          <input
+            type="checkbox"
+            value="presence"
+            v-model="showSamplesLayer"
+            @click="toggleSamplesLayer"
+          />
+          <svg height="30" width="30" @click="toggleSamplesLayer">
+            <circle
+              cx="15"
+              cy="15"
+              r="7"
+              stroke="#222"
+              stroke-width="2"
+              fill="#5aa172"
+            />
+          </svg>
+          {{ samplesCount }} {{ "site" | pluralize(samplesCount) }}
+        </div>
+      </div>
+      <hr />
       <ol>
         <li v-for="place in places" v-bind:key="place.id">
           <a :href="'/places/' + place.id">{{ place.name }}</a>
@@ -25,7 +64,8 @@ import Spinner from "./shared/spinner";
 import baseMap from "../packs/base_map.js";
 import LaRiverBaseMap from "../packs/la_river_base_map.js";
 
-const placesEndpoint = `/api/v1/places_pour`;
+const placesEndpoint = "/api/v1/places_pour";
+const samplesEndpoint = "/api/v1/basic_samples";
 
 export default {
   name: "PlacesIndex",
@@ -39,10 +79,20 @@ export default {
     return {
       showSpinner: false,
       places: [],
+      placesData: [],
+      showPlacesLayer: true,
+      placesLayer: null,
+      placesCount: 0,
+      samples: [],
+      samplesData: [],
+      showSamplesLayer: false,
+      samplesLayer: null,
+      samplesCount: 0,
     };
   },
   created() {
     this.fetchPlaces();
+    this.fetchSamples();
   },
 
   mounted() {
@@ -60,18 +110,82 @@ export default {
         }
       });
     },
+    togglePlacesLayer: function () {
+      if (this.showPlacesLayer) {
+        this.removePlacesLayer();
+        this.showPlacesLayer = false;
+      } else {
+        this.addPlacesLayer();
+        this.showPlacesLayer = true;
+      }
+    },
+    addPlacesLayer: function () {
+      let layer = baseMap.createMarkerLayer(
+        this.placesData,
+        baseMap.createTriangleMarker
+      );
+      this.placesLayer = layer.addTo(this.map);
+    },
+    removePlacesLayer: function () {
+      if (this.showPlacesLayer) {
+        this.placesLayer.clearLayers();
+      }
+    },
+
+    toggleSamplesLayer: function () {
+      if (this.showSamplesLayer) {
+        this.removeSamplesLayer();
+        this.showSamplesLayer = false;
+      } else {
+        this.addSamplesLayer();
+        this.showSamplesLayer = true;
+      }
+    },
+    addSamplesLayer: function () {
+      let layer = baseMap.createMarkerLayer(
+        this.samplesData,
+        baseMap.createCircleMarker
+      );
+      this.samplesLayer = layer.addTo(this.map);
+    },
+    removeSamplesLayer: function () {
+      if (this.showSamplesLayer) {
+        this.samplesLayer.clearLayers();
+      }
+    },
+
     //================
-    // fetch places
+    // fetch data
     //================
-    fetchPlaces() {
+    fetchPlaces: function () {
       this.showSpinner = true;
       axios
         .get(placesEndpoint)
         .then((response) => {
           this.places = response.data.places;
-          this.places.forEach((place) => {
-            let data = LaRiverBaseMap.formatPlaceData(place);
-            baseMap.createTriangleMarker(data).addTo(this.map);
+          this.placesCount = this.places.length;
+
+          this.placesData = this.places.map((place) => {
+            return LaRiverBaseMap.formatPlaceData(place);
+          });
+          this.addPlacesLayer();
+
+          this.showSpinner = false;
+        })
+        .catch((e) => {
+          console.error(e);
+        });
+    },
+    fetchSamples: function () {
+      this.showSpinner = true;
+      axios
+        .get(samplesEndpoint)
+        .then((response) => {
+          this.samples = response.data.samples;
+          this.samplesCount = this.samples.length;
+
+          this.samplesData = this.samples.map((place) => {
+            return baseMap.formatSamplesData(place);
           });
 
           this.showSpinner = false;
