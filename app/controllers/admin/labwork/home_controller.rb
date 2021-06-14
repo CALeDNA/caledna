e6d0741848d0c50f2b742a9e6f400de124af38c2
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-06-14 14:47:36 -0700

    fix - prevent admin from crashing if Processing Samples page doesn't exist

--- app/controllers/admin/labwork/home_controller.rb
+++ app/controllers/admin/labwork/home_controller.rb
@@ -4,7 +4,7 @@ module Admin
   module Labwork
     class HomeController < Admin::ApplicationController
       def index
-        @page = Page.find_by(slug: 'admin-process-samples')
+        @page = Page.find_by(slug: 'admin-process-samples') || Page.new
       end
     end
   end
